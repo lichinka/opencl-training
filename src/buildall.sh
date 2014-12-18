7c57f0a8913b766c00db14f1406534ed7db1ca0d
Lucas Benedicic <benedicic@cscs.ch>
2014-12-18 08:22:17 +0100

    Added 'santis' to the list of supported systems.-

--- src/buildall.sh
+++ src/buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 
-# - On Daint, compile with this module setup:
+# - On Daint/Santis, compile with this module setup:
 #
 # module switch PrgEnv-cray PrgEnv-gnu
 # module load craype-accel-nvidia35
@@ -15,21 +15,30 @@ CLSRC=../src/kernels
 
 case $( hostname ) in 
     *daint*)
-        echo "Building on daint ..."
         CC=cc
         CXX=CC
         CLSDK=/opt/nvidia/cudatoolkit
         CLLIB=/opt/cray/nvidia/default
         ;;
+    *santis*)
+        CC="cc -g -O0"
+        CXX="CC -g -O0"
+        CLSDK=/opt/nvidia/cudatoolkit/default
+        CLLIB=${CLSDK}
+        ;;
     *opcode*)
-        echo "Building on opcode ..."
         CC=mpicc
         CXX=g++
         CLSDK=/apps/opcode/CUDA-5.5
         CLLIB=/apps/opcode/CUDA-5.5
         ;;
+    *)
+        echo "Unknown system: ${HOSTNAME}"
+        exit 1
+        ;;
 esac
 
+echo "Building on ${HOSTNAME} ..."
 $CXX $SRC/01_device_query.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 01_device_query
 $CXX $SRC/02_create_context.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 02_create_context
 $CXX $SRC/03_kernel_load_and_exec.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 03_kernel_load_and_exec
@@ -40,7 +49,7 @@ $CXX $SRC/07_convolution.cpp $SRC/clutil.cpp -I$CLSDK/include -L$CLLIB/lib64 -lO
 $CXX -DWRITE_TO_IMAGE $SRC/07_convolution.cpp $SRC/clutil.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 07_convolution_image_write
 $CXX $SRC/08_cpp.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 08_cpp
 $CXX $SRC/09_memcpy.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 09_memcpy
-$CC  $SRC/10_mpi.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 10_mpi
+$CXX $SRC/10_mpi.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o 10_mpi
 $CXX $SRC/cl-compiler.cpp $SRC/clutil.cpp -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o clcc
 $CC  -DPINNED $SRC/osu_bwidth.c -I$CLSDK/include -L$CLLIB/lib64 -lOpenCL -o osu_bwidth
 

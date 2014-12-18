d95a7d852a2261252c210ba41a9ff4427f16e91d
Lucas Benedicic <benedicic@cscs.ch>
2014-12-18 08:22:41 +0100

    Support for older version of 'gcc'.-

--- src/osu_bwidth.c
+++ src/osu_bwidth.c
@@ -50,7 +50,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /* CLErrorString from SHOC suite http://ft.ornl.gov/doku/shoc/start */
 
-inline const char *CLErrorString(cl_int err)
+static inline const char *CLErrorString(cl_int err)
 {
     switch (err)
     {
